4e11adf17fa8ef8fecad6ac77f9fbcf1c346e88d
Andremt425 <andretoerien425@gmail.com>
2025-06-20 22:12:53 +0200

    Added main funtion and folders

--- CSharp-challenges/Program.cs
+++ CSharp-challenges/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string? projectType;
+        Console.WriteLine("Welcome to my C# challenge project");
+
+        projectType = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(projectType)) 
+        {
+            Console.WriteLine($"You entered: {projectType}");
+        }
+        else 
+        {
+            Console.WriteLine("You did not enter a valid project type.");
+        }
+    }
+}   
